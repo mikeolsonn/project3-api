4349fc90cc8a2327732856be29e0bf104dec16e2
Mike Olson <mikeolsonn97@gmail.com>
2021-01-09 14:51:54 -0500

    adds signup route

---
+++ routes/api/users.js
@@ -0,0 +1,8 @@
+// requires modules & set up router object
+const router = require('express').Router();
+// require the controller
+const usersCtrl = require('../../controllers/users');
+// define a signup route
+router.post('/signup', usersCtrl.signup);
+// export your router
+module.exports = router;
